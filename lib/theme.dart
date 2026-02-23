2873586be9477584f38485b20ad996937c999bc0
GakuruAlex <gakurualexgicimo@gmail.com>
2026-02-23 10:38:54 +0300

    App Theme
    Add colorsheme, textThem, appBarThem, floatingActionButtonTheme

---
+++ lib/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final darkPrimaryColor = Color.fromARGB(125, 98, 5, 198);
+final lightPrimaryColor = Color.fromARGB(125, 124, 61, 175);
+final primaryTextColor = Color.fromARGB(255, 255, 255, 255);
+final accentColor = Color.fromARGB(219, 218, 170, 15);
+
+final themeData = ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: darkPrimaryColor,
+    brightness: Brightness.dark,
+  ),
+  textTheme: TextTheme(
+    displayLarge: GoogleFonts.acme(fontSize: 24, fontStyle: FontStyle.italic),
+    bodyLarge: GoogleFonts.acme(
+      fontSize: 20,
+      fontStyle: FontStyle.normal,
+      color: primaryTextColor,
+    ),
+  ),
+  scaffoldBackgroundColor: darkPrimaryColor,
+  appBarTheme: AppBarTheme(backgroundColor: lightPrimaryColor),
+  floatingActionButtonTheme: FloatingActionButtonThemeData(
+    backgroundColor: accentColor,
+    foregroundColor: primaryTextColor,
+  ),
+);
